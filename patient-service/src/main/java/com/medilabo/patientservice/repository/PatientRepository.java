3145277b72b760657d5f048382feb5bcd79f6eab
achahbounehamid <hamid.achahboune@gmail.com>
2025-06-08 15:18:29 +0200

    main du Sprint 1

--- patient-service/src/main/java/com/medilabo/patientservice/repository/PatientRepository.java
+++
@@ -1,8 +0,0 @@
-package com.medilabo.patientservice.repository;
-
-import com.medilabo.patientservice.model.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientRepository extends JpaRepository<Patient, Long>{
-
-}
