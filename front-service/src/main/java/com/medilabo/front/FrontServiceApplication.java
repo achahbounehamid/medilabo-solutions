cbc9d50576edd181a5626c3faf70b526baefbb34
achahbounehamid <hamid.achahboune@gmail.com>
2025-06-18 16:02:21 +0200

    FrontController

--- front-service/src/main/java/com/medilabo/front/FrontServiceApplication.java
+++ front-service/src/main/java/com/medilabo/front/FrontServiceApplication.java
@@ -1,5 +1,6 @@
 package com.medilabo.front;
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FrontServiceApplication {
--- front-service/src/main/java/com/medilabo/front/config/SecurityConfig.java
+++ front-service/src/main/java/com/medilabo/front/config/SecurityConfig.java
@@ -1,8 +1,12 @@
 package com.medilabo.front.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -24,6 +28,17 @@ public class SecurityConfig {
                         .permitAll()
                 );
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService users() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
     }
 
 }
